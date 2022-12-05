bfcef686a57362345ebefb31406e291460b52b6d
Eric Hochwald <erichochwald@Erics-MacBook-Pro.local>
2022-12-04 14:57:04 -1000

    created a route to get all of a user's thoughts

---
+++ server/routes/user-routes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+
+const AWS = require('aws-sdk');
+const awsConfig = { region: 'us-east-2' };
+AWS.config.update(awsConfig);
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const table = "Thoughts";
+
+// retrieves all the user's thoughts from the thoughts table
+router.get('/users', (req, res) => {
+    const params = {
+        TableName: table,
+    };
+    // scan returns all items in a table
+    dynamodb.scan(params, (err, data) => {
+        if (err) {
+            res.status(500).json(err); 
+        } else {
+            res.json(data.Items);
+        }
+    });
+});
+
+router.get('/users/:username', (req, res) => {
+    const username = req.params.username;
+    console.log(`Querying for thoughts from ${username}.`);
+    const params = {
+        TableName: table,
+        // Similar to the WHERE clause in SQL, the KeyConditionExpression property is used to filter the query with an expression
+        // The #un and :user symbols are aliases that represent the attribute name and value
+        KeyConditionExpression: '#un = :user',
+        // the attribute name aliases have the # prefix
+        ExpressionAttributeNames: {
+            '#un': 'username',
+            '#ca': 'createdAt',
+            '#th': 'thought',
+        },
+        // the value aliases
+        ExpressionAttributeValues: {
+            ':user': username,
+        },
+        // this determines which attributes or columns get returned
+        ProjectionExpression: '#th, #ca',
+        // which specifies the order for the sort key (descending), default is true which is ascending order
+        ScanIndexForward: false,
+    };
+    dynamodb.query(params, (err, data) => {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            res.status(500).json(err);
+        } else {
+            console.log("Query suceeded");
+            res.json(data.Items);
+        }
+    });
+});
+
+module.exports = router;

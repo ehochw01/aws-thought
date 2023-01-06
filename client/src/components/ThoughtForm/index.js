3ff5f1fec8f384dfe598bbb44635ce2b992f844e
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2023-01-06 15:45:19 -0500

    added some error checking for trying to upload a photo without choosing a file

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -20,9 +20,15 @@ const ThoughtForm = () => {
   const handleImageUpload = (event) => {
     event.preventDefault();
     const data = new FormData();
+    console.log("fileInput.current.files[0]: ", fileInput.current.files[0]);
+    if (fileInput.current.files[0] === undefined) {
+      alert("You must choose a file before uploading");
+      return;
+    }
     data.append('image', fileInput.current.files[0]);
   
     const postImage = async () => {
+      document.getElementById("upload-btn").disabled = true;
       try {
         const res = await fetch('/api/image-upload', {
           mode: 'cors',
@@ -31,10 +37,14 @@ const ThoughtForm = () => {
         });
         if (!res.ok) throw new Error(res.statusText);
         const postResponse = await res.json();
+        alert("Uploaded");
+        document.getElementById("upload-btn").disabled = false;
         setFormState({ ...formState, image: postResponse.Location });
         console.log('postImage: ', postResponse.Location);
         return postResponse.Location;
       } catch (error) {
+        alert("Upload failed");
+        document.getElementById("upload-btn").disabled = false;
         console.log(error);
       }
     };
@@ -44,7 +54,10 @@ const ThoughtForm = () => {
   // submit form
   const handleFormSubmit = (event) => {
     event.preventDefault();
-
+    if (formState.username === "") {
+      alert("Enter a username");
+      return;
+    }
     const postData = async() => {
       const res = await fetch('api/users', {
         method: 'POST',
@@ -61,6 +74,7 @@ const ThoughtForm = () => {
     // clear form value
     setFormState({ username: "", thought: "" });
     setCharacterCount(0);
+    window.location.reload();
   };
 
   return (
@@ -89,7 +103,7 @@ const ThoughtForm = () => {
         <label className="form-input col-12  p-1">
           Add an image to your thought:
           <input type="file" ref={fileInput} className="form-input p-2" />
-          <button className="btn" onClick={handleImageUpload} type="submit">
+          <button className="btn" id="upload-btn" onClick={handleImageUpload} type="submit">
             Upload
           </button>
         </label>
